c9ddaf4a9266d2156351ef2ab056a7083fc434ef
LoopFruits <Kensworldc@gmail.com>
2023-09-29 15:01:57 -0400

    created map component and added qc map

---
+++ Open-Source/src/components/CampusMap.jsx
@@ -0,0 +1,36 @@
+import QCMap from '../assets/QCMap.png';
+
+
+
+const CampusMap = () => {
+
+    return(
+
+        <div>
+            <img 
+            src={QCMap}
+            alt="Queens College Campus Map"
+            loading= "lazy"
+            />
+        </div>
+    
+
+    )
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default CampusMap; 
