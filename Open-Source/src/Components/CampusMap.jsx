1db48b4ec68b9aa9097d99ff079042510ebe130d
LoopFruits <Kensworldc@gmail.com>
2023-09-30 14:47:15 -0400

    merged front end files

--- Open-Source/src/Components/CampusMap.jsx
+++ Open-Source/src/Components/CampusMap.jsx
@@ -6,7 +6,7 @@ const CampusMap = () => {
     const center = [ 40.736329, -73.820070]; // Lat, Long for initial map center (change as needed)
 
     return (
-        <MapContainer center={center} zoom={13} style={{ height: "500px", width: "100%" }}>
+        <MapContainer center={center} zoom={20} style={{ height: "500px", width: "100%" }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // Url for fetching tile from OpenStreetMap
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
