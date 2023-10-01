a3e70fb203275b06d6688b8197e54323d9f11137
shamblez2k <shalif.shaoul@gmail.com>
2023-10-01 00:52:16 -0400

    Fix: Finally got access to the DB through the code, and fixed the issues

--- Backend/bathrooms.js
+++ Backend/bathrooms.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8ldtugv.mongodb.net/Bathrooms?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@data.5a0gibq.mongodb.net/qc`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -22,22 +22,30 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Define the Bathroom Schema [Just an sample ]
-const bathroomSchema = new mongoose.Schema({
-  name: String,
-  location: String,
+// Define the bathrooms Schema [Just an sample ]
+const bathroomSchema = new mongoose.Schema(
+  {
+  building: String,
+  floor: Number,
+  gender: String,
   cleanRating: Number,
+  open: String,
   crowdedRating: Number,
-  __v: Number,
-});
+},
+{
+  collection: 'bathroom'
+}
+);
 
 
-const Bathrooms = mongoose.model('bathrooms', bathroomSchema);
+const Bathroom = mongoose.model('bathrooms', bathroomSchema);
 
 
 
 // API Routes
 // routes help us redirect or deliver data to the client
+
+//To get a list of all the bathroomss
 app.get(`/` , async (req, res) => {
   res.send(`Home`);
 });
@@ -45,30 +53,47 @@ app.get(`/` , async (req, res) => {
 
 app.get('/bathrooms', async (req, res) => {
   try {
-    const bathroom = await Bathrooms.find();
-    res.json(bathroom);
+    const bathrooms = await Bathroom.find();
+
+    res.json(bathrooms);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
   }
 });
 
+//To get a specific bathrooms, based on it's unique ID
+app.get('/bathrooms/:id', async (req, res) => {
+  try {
+      const bathrooms = await Bathroom.findById(req.params.id);
+      if (bathrooms) {
+          res.json(bathrooms);
+      } else {
+          res.status(404).json({ error: 'bathrooms not found' });
+      }
+  } catch (err) {
+      res.status(500).json({ error: 'Server error' });
+  }
+});
 
-// Handle POST requests to /bathrooms [ again this can be different]
+
+// Handle POST requests to /bathrooms
 app.post('/bathrooms', async (req, res) => {
   try {
     // Post methods store data in their body
     // using the req.body object we can retrieve this data
 
-    const { name, location, cleanRating, crowdedRating } = req.body;
-    const bathroom = new Bathrooms({
-      name,
-      location,
+    const { building, floor, gender, cleanRating, open, crowdedRating } = req.body;
+    const bathrooms = new Bathroom({
+      building,
+      floor,
+      gender,
       cleanRating,
+      open,
       crowdedRating,
     });
-    await bathroom.save();
-    res.status(201).json(bathroom);
+    await bathrooms.save();
+    res.status(201).json(bathrooms);
   } catch (err) {
     res.status(400).json({ error: 'Invalid data' });
   }
@@ -85,26 +110,48 @@ app.put('/bathrooms/:id', async (req, res) => {
     }
 
     try {
-        // Updating the bathroom with the provided ID
-        const updatedBathroom = await Bathrooms.findByIdAndUpdate(id, {
+        // Updating the bathrooms with the provided ID
+        const updatedbathrooms = await Bathroom.findByIdAndUpdate(id, {
             $set: {
                 ...(cleanRating !== undefined && { cleanRating }),
                 ...(crowdedRating !== undefined && { crowdedRating }),
             }
         }, { new: true });  // This option ensures the updated document is returned
 
-        // If the bathroom with the given ID isn't found, return a 404 error
-        if (!updatedBathroom) {
-            return res.status(404).json({ error: 'Bathroom not found' });
+        // If the bathrooms with the given ID isn't found, return a 404 error
+        if (!updatedbathrooms) {
+            return res.status(404).json({ error: 'bathrooms not found' });
         }
 
-        // Send the updated bathroom as the response
-        res.json(updatedBathroom);
+        // Send the updated bathrooms as the response
+        res.json(updatedbathrooms);
 
     } catch (err) {
         res.status(500).json({ error: 'Server error' });
     }
 });
+
+
+// app.get('/rating2', async (req, res) =>{
+//   try {
+
+//     const bathrooms = ( await bathrooms.aggregate([
+//       //Stage 3: Group remaining documents by item name and sum
+//       {
+//         '$group':{
+//            _id:"$location", totalCleanRating:{
+//             $sum: '$cleanRating'
+//             }
+//         }
+//       }
+//     ]
+//     ));
+//     console.log(bathrooms);
+//     res.json(bathrooms);
+//   } catch (err) {
+//     res.status(500).json({ error: 'Server error' });
+//   }
+// });
 
 
 // Start the local Server
--- Backend/users.js
+++ Backend/users.js
@@ -1,125 +1,125 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-//const session = require('express-session');
-//const crypto = require('crypto');
-//const MongoDBStore = require('connect-mongodb-session')(session);
-//npm install express-session connect-mongodb-session
-//npm install bcrypt
+// const mongoose = require('mongoose');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const bcrypt = require('bcrypt');
+// //const session = require('express-session');
+// //const crypto = require('crypto');
+// //const MongoDBStore = require('connect-mongodb-session')(session);
+// //npm install express-session connect-mongodb-session
+// //npm install bcrypt
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.json());
-console.log("Hello World");
+// // Middleware
+// app.use(bodyParser.json());
+// console.log("Hello World");
 
-const User = mongoose.model('User', {
-    email: String,
-    password: String,
-});
+// const User = mongoose.model('User', {
+//     email: String,
+//     password: String,
+// });
 
-//const store = new MongoDBStore({
-    //This uri will change, need to create a section for sessions in MongoDB
-    //uri: 'mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8ldtugv.mongodb.net/Bathrooms?retryWrites=true&w=majority',
-    //collection: 'sessions',
-//});
+// //const store = new MongoDBStore({
+//     //This uri will change, need to create a section for sessions in MongoDB
+//     //uri: 'mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8ldtugv.mongodb.net/Bathrooms?retryWrites=true&w=majority',
+//     //collection: 'sessions',
+// //});
 
-//Tests if there are errors in the session store
-//store.on('error', function(error){
-    //console.error('Session Store Error:', error);
-//});
+// //Tests if there are errors in the session store
+// //store.on('error', function(error){
+//     //console.error('Session Store Error:', error);
+// //});
 
-//app.use is for things that occur between a request and response
-//This session creation will occur between submitting login info and logging in 
-//app.use(
-    //session({
-        //Generates a session key for each session
-        //secret: crypto.randomBytes(16).toString('hex'),
-        //resave: false,
-        //saveUninitialized: false,
-    //})
-//)
+// //app.use is for things that occur between a request and response
+// //This session creation will occur between submitting login info and logging in 
+// //app.use(
+//     //session({
+//         //Generates a session key for each session
+//         //secret: crypto.randomBytes(16).toString('hex'),
+//         //resave: false,
+//         //saveUninitialized: false,
+//     //})
+// //)
 
-//const isAuthenticated = (req, res, next) => {
-//    if(req.session.isAuthenticated) {
-//        return next();
-//    }
-//    res.status(401).json({ error: 'Unauthorized. Please Log in' });
-//};
+// //const isAuthenticated = (req, res, next) => {
+// //    if(req.session.isAuthenticated) {
+// //        return next();
+// //    }
+// //    res.status(401).json({ error: 'Unauthorized. Please Log in' });
+// //};
 
-app.post('/register', async (req, res) => {
-    try{
-        //Data is retreieved from the frontend and stored into the variables
-        const{ username, email, password } = req.body;
+// app.post('/register', async (req, res) => {
+//     try{
+//         //Data is retreieved from the frontend and stored into the variables
+//         const{ username, email, password } = req.body;
 
-        //Check if email is already in the database
-        //Uses findOne because we assume that only one version of the email exists in the database
-        const userExists = await User.findOne({ email });
-        if(userExists) {
-            //Email already exists, sends 409 error (conflicts with existing conditions) and sends error message
-            return res.status(409).json({ error: 'This email has already been registered' });
-        }
+//         //Check if email is already in the database
+//         //Uses findOne because we assume that only one version of the email exists in the database
+//         const userExists = await User.findOne({ email });
+//         if(userExists) {
+//             //Email already exists, sends 409 error (conflicts with existing conditions) and sends error message
+//             return res.status(409).json({ error: 'This email has already been registered' });
+//         }
 
-        //Check if username is already in the database
-        const usernameExists = await User.findOne({ username });
-        if(usernameExists){
-            //A person already has the username, prompts to choose another
-            return res.status(409).json({ error: 'Someone already has this username' });
-        }
+//         //Check if username is already in the database
+//         const usernameExists = await User.findOne({ username });
+//         if(usernameExists){
+//             //A person already has the username, prompts to choose another
+//             return res.status(409).json({ error: 'Someone already has this username' });
+//         }
 
-        //Hashes the password for security
-        const hashPassword = await bycrypt.hash(password, 10);
+//         //Hashes the password for security
+//         const hashPassword = await bycrypt.hash(password, 10);
 
-        //User's information is stored into the MongoDB database
-        const newUser = new User({
-            username,
-            email,
-            password: hashPassword,
-        });
-        await newUser.save();
+//         //User's information is stored into the MongoDB database
+//         const newUser = new User({
+//             username,
+//             email,
+//             password: hashPassword,
+//         });
+//         await newUser.save();
 
-        res.status(200).json({ message: 'Registration successful!' });
-    }
-    catch  (err) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+//         res.status(200).json({ message: 'Registration successful!' });
+//     }
+//     catch  (err) {
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// });
 
-app.post('/login', async (req, res) => {
-    //Data is retreieved from the frontend and stored into the variables
-    const { email, password } = req.body;
+// app.post('/login', async (req, res) => {
+//     //Data is retreieved from the frontend and stored into the variables
+//     const { email, password } = req.body;
 
-    try{
-        //Looks for the email in the database
-        const user = await User.findOne({ email });
-        if(!user){
-            return res.status(401).json({ error: 'Invalid Credentials' });
-        }    
-        //Compares passwords
-        const passwordMatches = await bcrypt.compare(password, user.password);
-        if(!passwordMatch){
-            return res.status(401).json({ error: 'Invalid Credentials' });
-        }
-        //If sessions used
-        //req.session.isAuthenticated = true;
-        //req.session.username = user.username;
+//     try{
+//         //Looks for the email in the database
+//         const user = await User.findOne({ email });
+//         if(!user){
+//             return res.status(401).json({ error: 'Invalid Credentials' });
+//         }    
+//         //Compares passwords
+//         const passwordMatches = await bcrypt.compare(password, user.password);
+//         if(!passwordMatch){
+//             return res.status(401).json({ error: 'Invalid Credentials' });
+//         }
+//         //If sessions used
+//         //req.session.isAuthenticated = true;
+//         //req.session.username = user.username;
         
-        //I added the username in the response in case you want to extract it and use it to display their name in the corner or use it for something else
-        //If not needed, will remove
-        const username = user.username;
-        res.status(200).json({ username, message: 'Login Successful' });
-    }
-    catch (err) {
-        res.status(500).json({ error: 'Interal Server Error' });
-    }
-});
+//         //I added the username in the response in case you want to extract it and use it to display their name in the corner or use it for something else
+//         //If not needed, will remove
+//         const username = user.username;
+//         res.status(200).json({ username, message: 'Login Successful' });
+//     }
+//     catch (err) {
+//         res.status(500).json({ error: 'Interal Server Error' });
+//     }
+// });
 
-app.post('/reviews', isAuthenticated, async (req, res) => {
+// app.post('/reviews', isAuthenticated, async (req, res) => {
     
-});
+// });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
